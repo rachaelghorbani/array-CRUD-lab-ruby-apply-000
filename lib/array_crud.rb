4155b86af110a7a00fdef1f2a00a55687d4823c1
Rachael Ghorbani <rachael.ghorbani@gmail.com>
2020-07-29 19:52:24 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -15,13 +15,13 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  array.pop(element)
-  array
+  array.pop
+  
 end
 
 def remove_element_from_start_of_array(array)
-  array.shift(element)
-  array
+  array.shift
+  
 end
 
 def retrieve_element_from_index(array, index_number)
